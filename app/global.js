b141488fc155e7d6784db4cb5d1afce3361ba51f
Daniel Blythe <d.blythe@explori.com>
2020-04-15 13:49:05 +0100

    added global font Lato

--- app/global.js
+++ app/global.js
@@ -1,4 +1,11 @@
 const global = {
+    '@font-face': {
+        fontFamily: 'Lato',
+        src: 'url(\'/fonts/Lato-Regular.ttf\') format(\'truetype\'), url(\'/fonts/Lato-Bold.ttf\') format(\'truetype\')',
+    },
+    html: {
+        fontFamily: 'Lato',
+    },
     p: {
         color: '#ccc !important'
     },
