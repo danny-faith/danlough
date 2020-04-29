2745bafdc1d73960ba1c33ad7c1d7a74f59ffa78
Daniel Blythe <d.blythe@explori.com>
2020-04-29 16:17:33 +0100

    added utilities comp

---
+++ app/components/Utilities.js
@@ -0,0 +1,29 @@
+import styled from '@emotion/styled'
+
+const StyledUtilities = styled.ul`
+    display: flex;
+    justify-content: flex-end;
+    width: 15vw;
+
+    li {
+        list-style: none;
+
+        a {
+            font-size: 1.1rem;
+            font-weight: 700;
+            margin: 0 10px;
+            padding: 10px;
+            text-decoration: none;
+        }
+    }
+`
+
+export default () => {
+    return (
+        <StyledUtilities>
+            {/* <li><a><i className="fas fa-shopping-cart"></i></a></li> */}
+            <li><a>Search</a></li>
+            <li><a>Cart</a></li>
+        </StyledUtilities>
+    )
+}
