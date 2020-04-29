de431f2974a310bacd03e0e4eca797d2cd07ae15
Daniel Blythe <d.blythe@explori.com>
2020-04-29 16:19:57 +0100

    added logo comp

---
+++ app/components/Logo.js
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled'
+import theme from '../theme'
+
+const StyledLogo = styled.div`
+    margin-left: ${theme.padding.base};
+    width: 15vw;
+`
+
+const StyleImage = styled.img`
+    height: auto;
+    width: 110px;
+`
+
+export default () => {
+    return (
+        <StyledLogo>
+            <StyleImage src="/static/logo.png" />
+        </StyledLogo>
+    )
+}
