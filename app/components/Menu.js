40cd6709347427ba2678c65598e903f201a3c193
Daniel Blythe <d.blythe@explori.com>
2020-04-29 16:20:06 +0100

    added menu comp

---
+++ app/components/Menu.js
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+import styled from '@emotion/styled'
+import theme from '../theme'
+import { darken } from 'polished'
+
+const StyledMenu = styled.div`
+    display: flex;
+    flex-grow: 2;
+    justify-content: center;
+    padding: 30px;
+
+    a {
+        font-size: 1.1rem;
+        font-weight: 700;
+        margin: 0 10px;
+        padding: 10px;
+        text-decoration: none;
+    }
+`
+
+export default () => (
+  <StyledMenu>
+    <Link href="/">
+      <a>Home</a>
+    </Link>
+    <Link href="/recipes">
+      <a>Recipes</a>
+    </Link>
+    <Link href="/products">
+      <a>Products</a>
+    </Link>
+  </StyledMenu>
+);
