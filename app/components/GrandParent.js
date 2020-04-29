a2a5f99eba60413e9cd5b440060c3dfcd929e507
Daniel Blythe <d.blythe@explori.com>
2020-04-29 16:19:37 +0100

    temp grandparent & parent comp

--- app/components/GrandParent.js
+++ app/components/GrandParent.js
@@ -1,8 +1,31 @@
 import Parent from "./Parent"
+import Context from '../Context'
+import { useContext } from 'react'
+import { observer } from "mobx-react"
 
+const GrandParent = observer((props) => {
+    const context = useContext(Context)
+    const { tasks } = context
+    return (
+        <React.Fragment>
+            <Parent>Parent</Parent>
+            <ul>
+                {tasks.map((task) => <li key={task}>{task}</li>)}
+            </ul>
+        </React.Fragment>
+    )
+})
 
-const GrandParent = (props) => {
-    return <Parent>{props.children}</Parent>
-}
+// class GrandParent extends React.Component {
+//     render() {
+//         const { tasks } = this.context
+//         return (
+//             <ul>
+//                 {tasks.map((task) => <li key={task}>{task}</li>)}
+//             </ul>
+//         )
+//     }
+// }
+// GrandParent.contextType = Context
 
 export default GrandParent
--- app/components/Parent.js
+++ app/components/Parent.js
@@ -1,5 +1,23 @@
-const Parent = (props) => {
-    return <div>{props.children}</div>
-}
+import { observer } from "mobx-react"
+import Context from '../Context'
+import { useContext } from 'react'
+
+const Parent = observer((props) => {
+    const context = useContext(Context)
+    const { tasks } = context
+    const addTodo = (todo) => {
+        console.log('hello');
+        context.add(`seven_${Math.floor(Math.random() * 10)}`)
+    }
+    return (
+        <div>
+            <p>{props.children}</p>
+            <ul>
+                {tasks.map((task) => <li key={task}>{task}</li>)}
+            </ul>
+            <button onClick={addTodo}>Add todo</button>
+        </div>
+    )
+})
 
 export default Parent
